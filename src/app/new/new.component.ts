b256ab5bf3743e47a948a3e75ebaf64aadedc68a
venkatesh_g <venkatesh.g@mtwlabs.com>
2018-05-22 10:19:42 +0530

    updating ngstyle and ngClass

--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -9,18 +9,30 @@ import { Component, OnInit } from '@angular/core';
 
 export class NewLearning implements OnInit {
     public display : any = [];
+    public dynamicStyle;
 
     ngOnInit(){
        this.display = [{
+        name : 'adarsh',
+        age : 30,
+        isAged : 'aged'
+    },{
            name : 'nagesh',
-            age :22
-       },{
-           name : 'adarsh',
-           age : 30
+            age :22,
+            isAged : 'notAged'
        },{
            name : '',
-           age : 25
+           age : 25,
+           isAged : 'notAged'
        }] 
+    }
+
+    getHeight(){
+         
+        return {
+            'height' : window.innerHeight + 'px',
+            'background-color' : 'red'
+        } ;
     }
 
     }
